e9f6b3948dc0d9786178f630817b8019719c162b
Zuleny <zuleny.cr@gmail.com>
2025-06-29 15:30:56 -0400

    delete */obj folders, consumer

---
+++ NutritionalDelibery.Infrastructure/RabbitMQ/Consumers/LabeledPackageConsumer.cs
@@ -0,0 +1,34 @@
+﻿using Joseco.Communication.External.Contracts.Services;
+using MediatR;
+using NutritionalDelibery.Application.ExitNote.CreateExitNoteDetail;
+using NutritionalDelibery.Application.ExitNoteDetail.CreateExitNote;
+using NutritionalDelibery.Domain.ExitNote;
+using NutritionalDelibery.Integration.Package;
+
+namespace NutritionalDelibery.Infrastructure.RabbitMQ.Consumers
+{
+    public class LabeledPackageConsumer(IMediator mediator) : IIntegrationMessageConsumer<LabeledPackage>
+    {
+        public async Task HandleAsync(LabeledPackage message, CancellationToken cancellationToken)
+        {
+            Random random = new Random();
+
+            CreateExitNoteCommand command = new CreateExitNoteCommand(
+                random.Next(),
+                "",
+                DateTime.UtcNow,
+                Guid.NewGuid()
+            );
+
+            var exitNoteId = await mediator.Send(command, cancellationToken);
+
+            CreateExitNoteDetailCommand commandDetail = new CreateExitNoteDetailCommand(
+                1, 
+                message.PackageId, 
+                exitNoteId
+            );
+
+            await mediator.Send(commandDetail, cancellationToken);
+        }
+    }
+}
---
+++ NutritionalDelibery.Integration/Package/LabeledPackage.cs
@@ -0,0 +1,17 @@
+﻿using Joseco.Communication.External.Contracts.Message;
+
+namespace NutritionalDelibery.Integration.Package
+{
+    public record LabeledPackage : IntegrationMessage
+    {
+        public Guid PackageId { get; set; }
+        public string Status { get; set; } = string.Empty;
+
+        public LabeledPackage(Guid packageId, string status, string? correlationId = null, string? source = null)
+            : base(correlationId, source)
+        {
+            PackageId = packageId;
+            Status = status;
+        }
+    }
+}
